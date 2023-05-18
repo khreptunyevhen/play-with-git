29c6ca5fbd23b156840115bfe90b7d4bc8724d5f
Yevhen Khreptun <khreptun.e@gmail.com>
2023-05-18 10:56:29 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const obj = {
+  speed: 200,
+  type: "Audi",
+};
+
+const color = "grey";
+obj.color = color;
+
+console.log(obj);
+console.log(obj.speed);
+console.log(obj["speed"]);
+console.log(obj["color"]);
